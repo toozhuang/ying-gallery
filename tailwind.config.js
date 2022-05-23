7656b29639e94078bbe000d4aab5c0b308c87031
toozhuang <wangz.today@gmail.com>
2022-05-23 18:07:04 +0800

    chore: install tailwindcss and react-spring

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/**
+ * date: 2022-05-23, Mon, 17:6
+ * author: Wang
+ * feature： Tailwind css configure
+ */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
